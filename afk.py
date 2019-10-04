71a425d7f06a655807885145304b776604f3f5e7
Gabriel Sales <gabrielbsales@gmail.com>
2019-10-04 18:44:59 -0300

    Update afk.py
    Now the script reduces the cpu consumption when the game is running.

--- afk.py
+++ afk.py
@@ -8,64 +8,65 @@ import pyautogui
 
 LEAGUE_WINDOW_TITLE = 'League of Legends'
 LEAGUE_PROCESS_NAME = 'LeagueClient.exe'
+LEAGUE_GAME_NAME = 'League of Legends.exe'
 LEAGUE_PHASES = ['Lobby', 'Matchmaking', 'ReadyCheck', 'ChampSelect', 'GameStart', 'PostGame', 'ReadyCheckAccepted']
 
 class LeagueClient(object):
     def __init__(self):
-        self.hwnd = None #Window handle
+        self.hwnd = None
         self.x = 0
         self.y = 0
         self.w = 0
         self.h = 0
 
-        self.phase = None #Cliente gameflow phase
+        self.phase = None
 
-        self.process = None #Process object
-        self.logname = None #Log filename
+        self.process = None
+        self.logname = None
 
         # log attrs
-        self._last_size = 0 # Last log size
-        self._last_str = '' # Last log update
-        self._file_index = 0 # Current file position
+        self._last_size = 0
+        self._last_str = ''
+        self._file_index = 0
 
         self.update()
 
     def focus(self):
         try:
-            if win32gui.IsIconic(self.hwnd): # Check if client window is minimized
-                win32gui.ShowWindow(self.hwnd, 9) # If yes, then restore it
+            if win32gui.IsIconic(self.hwnd):
+                win32gui.ShowWindow(self.hwnd, 9)
                 return False
 
-            win32gui.SetForegroundWindow(self.hwnd) # Bring window to front
-            win32gui.SetActiveWindow(self.hwnd) # Active it
+            win32gui.SetForegroundWindow(self.hwnd)
+            win32gui.SetActiveWindow(self.hwnd)
         except:
             pass
         
-        return self._is_focused() # Return if the window is focused
+        return self._is_focused()
 
     def _is_focused(self):
-        return (win32gui.GetForegroundWindow() == self.hwnd) and (win32gui.IsWindowVisible(self.hwnd) == 1) # Check if window is visible
+        return (win32gui.GetForegroundWindow() == self.hwnd) and (win32gui.IsWindowVisible(self.hwnd) == 1)
 
     def _get_window(self):
-        self.hwnd = win32gui.FindWindow(None, LEAGUE_WINDOW_TITLE) # Find league client window
-        if self.hwnd <= 0: # If not found, set it to None
+        self.hwnd = win32gui.FindWindow(None, LEAGUE_WINDOW_TITLE)
+        if self.hwnd <= 0:
             self.hwnd = None
 
     def _get_window_info(self):
         if self.hwnd == None:
             return
         
-        rect = win32gui.GetWindowRect(self.hwnd) # Get window position and dimensions
+        rect = win32gui.GetWindowRect(self.hwnd)
         self.x = rect[0]
         self.y = rect[1]
         self.w = rect[2] - self.x
         self.h = rect[3] - self.y
 
     def _get_process(self):
-        self.process = None # Set process to None, so it won't pretend to exist when don't
-        for p in psutil.process_iter(attrs=['exe']): # Search process
-            if p.info['exe'] != None and p.info['exe'].endswith(LEAGUE_PROCESS_NAME): # Check if process exe matches league exe
-                self.process = p # Set it
+        self.process = None
+        for p in psutil.process_iter(attrs=['exe']):
+            if p.info['exe'] != None and p.info['exe'].endswith(LEAGUE_PROCESS_NAME):
+                self.process = p
 
     def _get_logname(self):
         if self.process == None:
@@ -74,10 +75,10 @@ class LeagueClient(object):
 
         try:
             # this returns the list of opened files by the current process
-            flist = self.process.open_files() # Get process open files
+            flist = self.process.open_files()
             if flist:
                 for nt in flist:
-                    if nt.path.endswith('LeagueClient.log'): # Check if the filename matches our log
+                    if nt.path.endswith('LeagueClient.log'):
                         self.logname = nt.path
 
         # This catches a race condition where a process ends
@@ -90,9 +91,9 @@ class LeagueClient(object):
             return False
 
         with open(self.logname) as f:
-            f.seek(self._file_index) # Seek to last position
-            self._last_str = f.read(self._last_size - self._file_index) # Get the changes
-            self._file_index = self._last_size # Update the last position
+            f.seek(self._file_index)
+            self._last_str = f.read(self._last_size - self._file_index)
+            self._file_index = self._last_size
         
         return True
 
@@ -100,8 +101,8 @@ class LeagueClient(object):
         if self.logname == None:
             return False
 
-        fsize = os.stat(self.logname).st_size # Get the current log size in bytes
-        if fsize != self._last_size: # If the size is different to the stored size, the update things
+        fsize = os.stat(self.logname).st_size
+        if fsize != self._last_size:
             self._last_size = fsize
             self._update_log()
             return True
@@ -109,31 +110,31 @@ class LeagueClient(object):
         return False
 
     def _parse_log(self):
-        lines = self._last_str.splitlines() # Split string of log changes into lines(list)
+        lines = self._last_str.splitlines()
 
-        for lineNumber in reversed(range(len(lines))): # Let's iterate over the lines from end to start
+        for lineNumber in reversed(range(len(lines))):
             line = lines[lineNumber]
-            phase = re.search(r'Gameflow: entering state \'(\w+)\'', line) # Find the gameflow changes
-            if phase != None: # Check if something was found
-                phase = phase.group(1) # Store it
-                if (self.phase != phase) and LEAGUE_PHASES.count(phase) > 0: # If the phase differs from the previous, then update the object
+            phase = re.search(r'Gameflow: entering state \'(\w+)\'', line)
+            if phase != None:
+                phase = phase.group(1)
+                if (self.phase != phase) and LEAGUE_PHASES.count(phase) > 0:
                     self.phase = phase
                     break
-            elif 'READY_CHECK_USER_ACCEPTED' in line: # Check if the match was accepted
+            elif 'READY_CHECK_USER_ACCEPTED' in line:
                 self.phase = 'ReadyCheckAccepted'
                 break
 
     def _accept_match(self):
-        button_x = self.x + (self.w * 0.511) # Calc the accept button position
+        button_x = self.x + (self.w * 0.511)
         button_y = self.y + (self.h * 0.765)
 
         pyautogui.click(x=button_x, y=button_y) 
 
     def try_accept_match(self):
-        if(self.focus()):# If the window was focused
-            self._accept_match() # Accept the match
+        if(self.focus()):
+            self._accept_match()
 
-    def log(self, p_phase): # Prettify the output, or not...
+    def log(self, p_phase):
         #['Lobby', 'Matchmaking', 'ReadyCheck', 'ChampSelect', 'GameStart', 'PostGame', 'ReadyCheckAccepted']
         if p_phase != self.phase:
             if self.phase == 'Matchmaking':
@@ -150,32 +151,49 @@ class LeagueClient(object):
                 print('>', 'Game finished')
             else:
                 print('>', self.phase)
-        
-    def update(self):
-        self._get_process() # Get the pid
-        self._get_logname() # Get the filename
 
-        p_phase = self.phase # Store previous phase
-        if self._log_has_changed(): # Check if the log has changed
-            self._parse_log() # Parse the changed log
+    def wait_game(self):
+        while True:
+            for p in psutil.process_iter(attrs=['exe']):
+                if p.info['exe'] != None and p.info['exe'].endswith(LEAGUE_GAME_NAME): # Find the game process, if it exists, we can continue the program
+                    break
+
+            time.sleep(5)
+            
+                
+
+    def update(self):
+        self._get_process()
+        self._get_logname()
+
+        p_phase = self.phase
+        if self._log_has_changed():
+            self._parse_log()
         
-        if self.phase == 'ReadyCheck': # If a match has found
-            self._get_window() # Get window pid
-            self._get_window_info() # Get window dimensions
-            self.try_accept_match() # Accept the match
+        if self.phase == 'ReadyCheck':
+            self._get_window()
+            self._get_window_info()
+            self.try_accept_match()
         
-        self.log(p_phase) # Print things
+        self.log(p_phase)
 
 def main():
+    sleep_time = 1
+    print(":> I am not afk!")
     league_client = LeagueClient()
     if league_client.process == None:
-        print("LeagueClient not running")
+        print(":< Because the client isn't running...")
         return None
-
-    print("I am not afk")
-    while league_client.process != None:
+    while league_client.process != None: # Keep running until the client is closed
         league_client.update()
-        time.sleep(0.5)
+        if league_client.phase == 'GameStart': # Check if the game was started
+            print('>', 'Waiting game end...')
+            league_client.wait_game() # Wait it finishes, reducing cpu consumption
+        elif league_client.phase == 'Matchmaking':
+            sleep_time = 0.5 # When in the queue, check faster for changes
+        else:
+            sleep_time = 1 # else, we can slow down
+
+        time.sleep(sleep_time)
 
 main()
-
